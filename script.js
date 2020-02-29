acf5b23c3baf1b58e08ca7c4a81ee35fb3ad5753
xaviguasch <xaviguasch@gmail.com>
2020-02-29 19:27:03 +0100

    gotten lyrics from an api request and displayed them in the dom

--- script.js
+++ script.js
@@ -55,6 +55,19 @@ function showData(data) {
   }
 }
 
+// lyrics for song
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`)
+  const data = await res.json()
+
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>')
+
+  result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+  <span>${lyrics}</span>`
+
+  more.innerHTML = ''
+}
+
 // Get prev and next songs
 async function getMoreSongs(url) {
   const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`)
@@ -73,5 +86,16 @@ form.addEventListener('submit', e => {
     alert('Please type in a search term')
   } else {
     searchSongs(searchTerm)
+  }
+})
+
+// Get lyrics button click
+result.addEventListener('click', e => {
+  const clickedEl = e.target
+  if (clickedEl.tagName === 'BUTTON') {
+    const artist = clickedEl.getAttribute('data-artist')
+    const songTitle = clickedEl.getAttribute('data-songtitle')
+
+    getLyrics(artist, songTitle)
   }
 })
